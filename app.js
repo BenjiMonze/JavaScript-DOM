3ded521b6cfaf83981ea6b99dfc263102c0d6ae8
BenjiMonze <ejben55@gmail.com>
2020-05-13 16:15:39 +0100

    Usig the Query Selector.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
 
-//Check wether HTML element titles is an array or not (Display true or fasle on console).
-console.log(Array.isArray(titles));
+//This querySelector only ever returns one element even if there are more than one.
+//It'll retrun the fist.
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
 
-//Convert the HTML collection(titles) to an array and then display if true or false on console.
-console.log(Array.isArray(Array.from(titles)));
+//The querySelectorAll would return a collection of elements.
+books = document.querySelectorAll('#book-list li .name');
 
-//Alternative way to cycle through an HTML collection rather than using for loop.
-Array.from(titles).forEach(function (item) {
-    console.log(item);
-})
+console.log(books);
