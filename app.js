1377ce8704a4b6fe3f77a361117443c8a1ef293a
BenjiMonze <ejben55@gmail.com>
2020-05-14 13:05:40 +0100

    Traversing the DOM (Part-1).

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const banner = document.querySelector('#page-banner');
+const banner = document.querySelector("#page-banner");
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner node has child nodes:', banner.hasChildNodes());
+console.log("#page-banner node type is:", banner.nodeType);
+console.log("#page-banner node name is:", banner.nodeName);
+console.log("#page-banner node has child nodes:", banner.hasChildNodes());
 
 const clonedBanner = banner.cloneNode(true);
 //If you don't pass in true, it won't give all the nested elements in the cloned element.
